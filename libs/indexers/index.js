768b2e95fd5b2dddd7a1630b60773851d6609c52
Froilan Irizarry <froilan@froilanirizarry.me>
2018-11-18 17:03:10 -0500

    Initial commit. Indexers package now exports all indexers in a single place

---
+++ libs/indexers/index.js
@@ -0,0 +1,9 @@
+const RepoIndexer = require('./repo');
+const TermIndexer = require('./term');
+const StatusIndexer = require('./status');
+
+module.exports = {
+  RepoIndexer,
+  TermIndexer,
+  StatusIndexer
+}
