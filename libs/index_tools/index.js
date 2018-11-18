e48c3f25beaa94ae7602a59e0fc3233a24ad7c75
Froilan Irizarry <froilan@froilanirizarry.me>
2018-11-18 16:59:47 -0500

    Added AbstractIndexer to exports

--- libs/index_tools/index.js
+++ libs/index_tools/index.js
@@ -1,9 +1,11 @@
 const { AliasSwapper } = require('./alias_swapper');
 const { IndexOptimizer } = require('./index_optimizer');
 const { IndexCleaner } = require('./index_cleaner');
+const { AbstractIndexer } = require('./abstract_indexer');
 
 module.exports = {
   AliasSwapper,
   IndexOptimizer,
-  IndexCleaner
+  IndexCleaner,
+  AbstractIndexer
 }
