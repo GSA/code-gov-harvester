5f625c83b9aea1ebd401d6da2a0063d6e6d01c34
Froilan Irizarry <froilan@froilanirizarry.me>
2018-12-10 15:03:04 -0500

    Fix github url regex and add stripBom static method

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -268,7 +268,7 @@ class Utils {
   }
 
   static isValidRepositoryUrl (repoUrl) {
-    const githubUrlRegEx = /(https|http):\/\/(www\.)?github.com\/[^\/]+\/[^\/]+$/g;
+    const githubUrlRegEx = /(https|http|git):\/\/(www\.)?github.com\/[^\/]+\/[^\/]+(.git)?$/g;
     if (repoUrl) {
       const match = repoUrl.match(githubUrlRegEx);
 
@@ -278,6 +278,15 @@ class Utils {
     }
 
     return false;
+  }
+
+  static stripBom(input) {
+    const inputString = input.toString();
+
+    if(inputString.charCodeAt(0) === 0xFEFF) {
+      return inputString.slice(1);
+    }
+    return inputString;
   }
 }
 
