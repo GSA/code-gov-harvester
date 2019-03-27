e8e358920501c39f54a6e546b51824c518d8e419
B J Bhatt <bhuvnesh.bhatt@nih.gov>
2019-03-27 10:59:46 -0400

    Add SMTP and GMail Mailer based on NodeMailer

---
+++ libs/mailer/index.js
@@ -0,0 +1,45 @@
+const nodemailer = require('nodemailer');
+
+class SMTPMailer {
+  constructor({host="localhost", port=587, secure=false, authType='login', user='', pass=''}) {
+    let options = {
+      host,
+      port,
+      secure,
+      auth: {
+        type: authType,
+        user,
+        pass
+      }
+    };
+    this.transporter = nodemailer.createTransport(options);
+  }
+
+  async sendMail({from, to, cc, bcc, subject, html}) {
+    await this.transporter.sendMail({
+      from, to, cc, bcc, subject, html
+    });
+  }
+}
+
+class GMailMailer {
+
+  constructor({ user, pass }) {
+    this.transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: { user, pass }
+    });
+  }
+
+  async sendMail({from, to, cc, bcc, subject, html}) {
+    await this.transporter.sendMail({
+      from, to, cc, bcc, subject, html
+    });
+  }
+
+}
+
+module.exports = {
+  SMTPMailer,
+  GMailMailer
+};
