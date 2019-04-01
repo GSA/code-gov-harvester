84cf1d0031448d04c545282c955afbfe2f435fef
B J Bhatt <bhuvnesh.bhatt@nih.gov>
2019-04-01 09:19:53 -0400

    Send Mail logic and HTML for sending E-Mail

--- libs/indexers/status/index.js
+++ libs/indexers/status/index.js
@@ -1,6 +1,7 @@
 const { AbstractIndexer } = require("../../index_tools");
 const crypto = require("crypto");
 const { Logger } = require('../../loggers');
+const { GMailMailer } = require("../../mailer");
 
 class StatusIndexer extends AbstractIndexer {
   get LOGGER_NAME() {
@@ -27,8 +28,71 @@ class StatusIndexer extends AbstractIndexer {
     });
   }
 
+  _convertToHTMLTRs({agencies, caption="", type=""}) {
+    if (agencies.length > 0) {
+      agencies.sort((a, b) => {
+        let acronymA = a.acronym.toLowerCase(), acronymB = b.acronym.toLowerCase();
+        return (acronymA < acronymB) ? -1 : (acronymA > acronymB) ? 1 : 0;
+      });
+      let bgColor = type.toLowerCase() === "error" ? "#F5A9A9"
+        : type.toLowerCase() === "warning" ? "#F2F5A9" 
+        : type.toLowerCase() === "duplicates" ? "#A9E2F3" 
+        : "#FFFFFF";
+      
+      let html = "";
+      if (caption) {
+        html = `<tr style="background-color: ${bgColor}; font-weight: bold">
+        <td colspan="5">${caption}</td>
+        </tr>`;        
+      }
+
+      agencies.forEach(agency => {
+        let counts = [];
+        Object.keys(agency.counts).forEach(usageType => {
+           counts.push(usageType + ": <em>" + agency.counts[usageType] + "</em>");
+        });
+        html += `<tr style="background-color: ${bgColor}">
+         <td>${agency.acronym}</td>
+         <td>${(agency.wasRemoteJsonRetrived ? "Yes" : "No")}</td>
+         <td>${(agency.wasRemoteJsonParsed ? "Yes" : "No")}</td>
+         <td>${(agency.wasFallbackUsed ? "Yes" : "No")}</td>
+         <td>${counts.join(" | ")}</td>
+         </tr>`;
+      });
+      return html;
+    }
+    return "";
+  }
+
+  getStatusHTML({report}) {
+    let agencies = [];
+    Object.keys(report.statuses).forEach(acronym => {
+      const { wasFallbackUsed, wasRemoteJsonRetrived, wasRemoteJsonParsed, counts } = report.statuses[acronym];
+      agencies.push({acronym, wasFallbackUsed, wasRemoteJsonRetrived, wasRemoteJsonParsed, counts});
+    });
+    let html = `<h3>CODE.GOV Repository Harvester Execution Summary</h3>
+      <h4>Time: ${report.timestamp}</h4>
+      <table border="1" cellspacing="0" cellpadding="2">
+        <tr style="background-color: #E6E6E6">
+        <th>Agency<br />Acronym</th>
+        <th>Retrieved Remote<br />code.json</th>
+        <th>Parsed Remote<br />code.json</th>
+        <th>Fallback<br />Used</th>
+        <th>Repository Counts</th> 
+        </tr>`;
+    html += this._convertToHTMLTRs({ agencies: agencies.filter(agency => !agency.wasRemoteJsonRetrived && !agency.wasFallbackUsed), caption: "No remote code.json file.  Missing fallback file or errors while processing fallback file.", type: "error" });
+    html += this._convertToHTMLTRs({ agencies: agencies.filter(agency => agency.wasRemoteJsonRetrived && !agency.wasRemoteJsonParsed && !agency.wasFallbackUsed), caption: "Errors while processing remote code.json file.  Missing fallback file or errors while processing fallback file.", type: "error" });
+    html += this._convertToHTMLTRs({ agencies: agencies.filter(agency => !agency.wasRemoteJsonRetrived && agency.wasFallbackUsed), caption: "No remote code.json file.  Used fallback file successfully.", type: "warning" });
+    html += this._convertToHTMLTRs({ agencies: agencies.filter(agency => agency.wasRemoteJsonRetrived && !agency.wasRemoteJsonParsed && agency.wasFallbackUsed), caption: "Errors while processing Remote code.json file.  Used fallback file successfully.", type:"warning" });
+    html += this._convertToHTMLTRs({ agencies: agencies.filter(agency => agency.wasRemoteJsonRetrived && agency.wasRemoteJsonParsed && agency.counts.duplicates), caption: "Used remote code.json successfully - with duplicates.", type: "duplicates" });
+    html += this._convertToHTMLTRs({ agencies: agencies.filter(agency => agency.wasRemoteJsonRetrived && agency.wasRemoteJsonParsed && !agency.counts.duplicates), caption: "Used remote code.json successfully." });
+    html += `</table>`;
+    return html 
+  }
+
   static async init(reporter, adapter, esParams, config) {
     const indexer = new StatusIndexer(adapter, esParams, config);
+    const html = indexer.getStatusHTML(reporter);
 
     indexer.logger.info(`Started indexing (${indexer.esType}) indices.`);
 
@@ -42,6 +106,15 @@ class StatusIndexer extends AbstractIndexer {
       await indexer.initMapping();
       await indexer.indexStatus(reporter);
 
+      try {
+        // TBD: To externalize user and password
+        const mailer = new GMailMailer({user: '****@gmail.com', pass: '*!*!*!*!*'});
+        // TBD: To externalize from, to, subject
+        await mailer.sendMail({from: '****@gmail.com', to: 'someone@some-domain.com', subject: "SUBJECT Details", html: html });
+      } catch(error) {
+        indexer.logger.error(error);
+        //throw error;  **Don't throw error if E-Mail cannot be sent **
+      }
       return { esIndex: indexInfo.index, esAlias: indexer.esAlias };
     } catch(error) {
       indexer.logger.error(error);
